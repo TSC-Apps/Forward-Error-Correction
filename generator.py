4af1625474e05353ad674918ca87ee3f832b1577
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-03-23 13:27:10 +0100

    Reduced 4 lines of for loop to 1 line of comprehenction

--- generator.py
+++ generator.py
@@ -3,7 +3,4 @@ import random
 
 # prosty generator
 def generate_bits(quantity):
-    lst = []
-    for i in range(0, quantity):
-        lst.append(random.randint(0, 1))
-    return lst
+    return [random.randint(0, 1) for i in range(0, quantity)]

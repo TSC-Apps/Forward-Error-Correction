11566581813d070c5038086801b4b0b99245a57f
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-03-24 10:11:28 +0100

    Added another great comprehention

--- coder.py
+++ coder.py
@@ -1,8 +1,3 @@
 # koder potrajający bit
 def triple_code(lst):
-    coded_lst = []
-    for i in lst:
-        for j in range(0, 3):
-            coded_lst.append(i)
-
-    return coded_lst
+    return [i for i in lst for j in range(0, 3)]
